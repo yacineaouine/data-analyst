fce8d63deab4fed2317fa82a6c3632e0a0ae61d1
unknown <yacine.aouine@outlook.fr>
2022-06-06 17:20:07 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("home.html")
+
+@app.route('/DWFA/')
+def dashboard():
+    return render_template("DWFA.html")
+
+@app.route('/tools/')
+def tools():
+    return render_template("tools.html")
+
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
